0d10ed1103e5d0d5990868b1bd11424e50bfa23d
GregoryMW <49440175+GregoryMW@users.noreply.github.com>
2023-03-12 23:30:21 -0400

    Added database file

---
+++ csci4050-ces.sql
@@ -0,0 +1,369 @@
+CREATE DATABASE  IF NOT EXISTS `csci4050-ces` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `csci4050-ces`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: csci4050-ces
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bookings`
+--
+
+DROP TABLE IF EXISTS `bookings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bookings` (
+  `bookingID` int NOT NULL AUTO_INCREMENT,
+  `userID` int NOT NULL,
+  `showID` int NOT NULL,
+  `paymentID` int NOT NULL,
+  `promoID` int NOT NULL,
+  `total` double NOT NULL,
+  PRIMARY KEY (`bookingID`),
+  KEY `bookings-userID_idx` (`userID`),
+  KEY `bookings-showID_idx` (`showID`),
+  KEY `bookings-paymentID_idx` (`paymentID`),
+  KEY `bookings-promoID_idx` (`promoID`),
+  CONSTRAINT `bookings-paymentID` FOREIGN KEY (`paymentID`) REFERENCES `paymentcards` (`paymentID`),
+  CONSTRAINT `bookings-promoID` FOREIGN KEY (`promoID`) REFERENCES `promotions` (`promoID`),
+  CONSTRAINT `bookings-showID` FOREIGN KEY (`showID`) REFERENCES `shows` (`showID`),
+  CONSTRAINT `bookings-userID` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookings`
+--
+
+LOCK TABLES `bookings` WRITE;
+/*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movies`
+--
+
+DROP TABLE IF EXISTS `movies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movies` (
+  `movieID` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `cast` varchar(255) NOT NULL,
+  `director` varchar(255) NOT NULL,
+  `producer` varchar(255) NOT NULL,
+  `synopsis` varchar(255) NOT NULL,
+  `review` varchar(255) NOT NULL,
+  `movieImage` varchar(255) NOT NULL,
+  `trailerVideo` varchar(255) NOT NULL,
+  PRIMARY KEY (`movieID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movies`
+--
+
+LOCK TABLES `movies` WRITE;
+/*!40000 ALTER TABLE `movies` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paymentcards`
+--
+
+DROP TABLE IF EXISTS `paymentcards`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `paymentcards` (
+  `paymentID` int NOT NULL AUTO_INCREMENT,
+  `userID` int NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `cardNumber` varchar(255) NOT NULL,
+  `expirationDate` varchar(255) NOT NULL,
+  `securityCode` varchar(255) NOT NULL,
+  PRIMARY KEY (`paymentID`),
+  KEY `paymentcards-userID_idx` (`userID`),
+  CONSTRAINT `paymentcards-userID` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paymentcards`
+--
+
+LOCK TABLES `paymentcards` WRITE;
+/*!40000 ALTER TABLE `paymentcards` DISABLE KEYS */;
+/*!40000 ALTER TABLE `paymentcards` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `promotions`
+--
+
+DROP TABLE IF EXISTS `promotions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `promotions` (
+  `promoID` int NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) NOT NULL,
+  `discount` double NOT NULL,
+  `startDate` varchar(255) NOT NULL,
+  `endDate` varchar(255) NOT NULL,
+  PRIMARY KEY (`promoID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `promotions`
+--
+
+LOCK TABLES `promotions` WRITE;
+/*!40000 ALTER TABLE `promotions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `promotions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seats`
+--
+
+DROP TABLE IF EXISTS `seats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `seats` (
+  `seatID` int NOT NULL AUTO_INCREMENT,
+  `roomID` int NOT NULL,
+  `available` int NOT NULL,
+  PRIMARY KEY (`seatID`),
+  KEY `seats-roomID_idx` (`roomID`),
+  CONSTRAINT `seats-roomID` FOREIGN KEY (`roomID`) REFERENCES `showrooms` (`roomID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seats`
+--
+
+LOCK TABLES `seats` WRITE;
+/*!40000 ALTER TABLE `seats` DISABLE KEYS */;
+/*!40000 ALTER TABLE `seats` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `showrooms`
+--
+
+DROP TABLE IF EXISTS `showrooms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `showrooms` (
+  `roomID` int NOT NULL AUTO_INCREMENT,
+  `roomName` varchar(255) NOT NULL,
+  `seatNum` int NOT NULL,
+  PRIMARY KEY (`roomID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `showrooms`
+--
+
+LOCK TABLES `showrooms` WRITE;
+/*!40000 ALTER TABLE `showrooms` DISABLE KEYS */;
+/*!40000 ALTER TABLE `showrooms` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shows`
+--
+
+DROP TABLE IF EXISTS `shows`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shows` (
+  `showID` int NOT NULL AUTO_INCREMENT,
+  `movieID` int NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `time` int NOT NULL,
+  `roomID` int NOT NULL,
+  PRIMARY KEY (`showID`,`date`,`time`,`roomID`),
+  KEY `movieID_idx` (`movieID`),
+  KEY `shows-roomID_idx` (`roomID`),
+  CONSTRAINT `shows-movieID` FOREIGN KEY (`movieID`) REFERENCES `movies` (`movieID`),
+  CONSTRAINT `shows-roomID` FOREIGN KEY (`roomID`) REFERENCES `showrooms` (`roomID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shows`
+--
+
+LOCK TABLES `shows` WRITE;
+/*!40000 ALTER TABLE `shows` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shows` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `status` (
+  `statusID` int NOT NULL AUTO_INCREMENT,
+  `status` varchar(255) NOT NULL,
+  PRIMARY KEY (`statusID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status`
+--
+
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+INSERT INTO `status` VALUES (1,'Active'),(2,'Inactive'),(3,'Suspended');
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tickets`
+--
+
+DROP TABLE IF EXISTS `tickets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tickets` (
+  `ticketID` int NOT NULL AUTO_INCREMENT,
+  `bookingID` int NOT NULL,
+  `typeID` int NOT NULL,
+  `seatID` int NOT NULL,
+  `price` double NOT NULL,
+  PRIMARY KEY (`ticketID`),
+  KEY `tickets-bookingID_idx` (`bookingID`),
+  KEY `tickets-typeID_idx` (`typeID`),
+  KEY `tickets-seatID_idx` (`seatID`),
+  CONSTRAINT `tickets-bookingID` FOREIGN KEY (`bookingID`) REFERENCES `bookings` (`bookingID`),
+  CONSTRAINT `tickets-seatID` FOREIGN KEY (`seatID`) REFERENCES `seats` (`seatID`),
+  CONSTRAINT `tickets-typeID` FOREIGN KEY (`typeID`) REFERENCES `tickettypes` (`typeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tickets`
+--
+
+LOCK TABLES `tickets` WRITE;
+/*!40000 ALTER TABLE `tickets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tickets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tickettypes`
+--
+
+DROP TABLE IF EXISTS `tickettypes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tickettypes` (
+  `typeID` int NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  PRIMARY KEY (`typeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tickettypes`
+--
+
+LOCK TABLES `tickettypes` WRITE;
+/*!40000 ALTER TABLE `tickettypes` DISABLE KEYS */;
+INSERT INTO `tickettypes` VALUES (1,'Adult'),(2,'Senior'),(3,'Child');
+/*!40000 ALTER TABLE `tickettypes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `userTypeID` int NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phoneNumber` int DEFAULT NULL COMMENT 'Admin users can have a null entry for a phone number',
+  `statusID` int NOT NULL,
+  PRIMARY KEY (`userID`),
+  KEY `users-userTypeID_idx` (`userTypeID`),
+  KEY `users-statusID_idx` (`statusID`),
+  CONSTRAINT `users-statusID` FOREIGN KEY (`statusID`) REFERENCES `status` (`statusID`),
+  CONSTRAINT `users-userTypeID` FOREIGN KEY (`userTypeID`) REFERENCES `usertype` (`typeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,2,'Admin','User','admin@admin.com','password',NULL,1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usertype`
+--
+
+DROP TABLE IF EXISTS `usertype`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usertype` (
+  `typeID` int NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  PRIMARY KEY (`typeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usertype`
+--
+
+LOCK TABLES `usertype` WRITE;
+/*!40000 ALTER TABLE `usertype` DISABLE KEYS */;
+INSERT INTO `usertype` VALUES (1,'Customer'),(2,'Admin');
+/*!40000 ALTER TABLE `usertype` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-12 23:28:56
